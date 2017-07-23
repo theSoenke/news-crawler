ac757c466d6a04e536a773df6bf01796421a377c
theSoenke <theSoenke@users.noreply.github.com>
2017-07-23 20:46:47 +0200

    Ensure feeds have been downloaded before running scraper

--- scripts/web-scraper.sh
+++ scripts/web-scraper.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+/usr/local/bin/news-crawler feeds /app/data/feeds_de.txt --timezone Europe/Berlin --out /app/out/feeds/
 /usr/local/bin/news-crawler scrape /app/out/feeds/ --timezone Europe/Berlin

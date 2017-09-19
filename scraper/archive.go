38b775488d4f56d43177c9fdd1e8813cab005f03
theSoenke <theSoenke@users.noreply.github.com>
2017-09-19 23:12:27 +0200

    Use hash for archived pages
    Removes error prone escaping of the filenames and too long file names

--- scraper/archive.go
+++ scraper/archive.go
@@ -1,10 +1,11 @@
 package scraper
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-	"strings"
 	"time"
 )
 
@@ -12,7 +13,6 @@ import (
 func (article *Article) Write(outDir string, dayTime *time.Time) error {
 	day := dayTime.Format("2-1-2006")
 	dayDir := filepath.Join(outDir, day)
-
 	if _, err := os.Stat(dayDir); os.IsNotExist(err) {
 		err := os.MkdirAll(dayDir, os.ModePerm)
 		if err != nil {
@@ -20,7 +20,8 @@ func (article *Article) Write(outDir string, dayTime *time.Time) error {
 		}
 	}
 
-	filename := strings.Replace(article.FeedItem.URL, "/", "\\", -1) + ".html"
+	hash := md5.Sum([]byte(article.FeedItem.URL))
+	filename := hex.EncodeToString(hash[:]) + ".html"
 	articlePath := filepath.Join(dayDir, filename)
 	err := ioutil.WriteFile(articlePath, []byte(article.HTML), 0644)
 	return err

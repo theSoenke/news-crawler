b172612038e83248f0e8d8b400209580cac3a06f
theSoenke <theSoenke@users.noreply.github.com>
2017-09-21 21:27:06 +0200

    Set language in docker

--- Dockerfile
+++ Dockerfile
@@ -12,8 +12,8 @@ RUN go get
 RUN go build -o /usr/local/bin/news-crawler
 
 RUN touch crontab.tmp \
-    && echo '30 * * * * /usr/local/bin/news-crawler feeds /app/data/feeds_de.txt --timezone Europe/Berlin --dir /app/out/feeds' > crontab.tmp \
-    && echo '0 2 * * * /usr/local/bin/news-crawler scrape /app/out/feeds/ --timezone Europe/Berlin --dir /app/out/articles' >> crontab.tmp \
+    && echo '30 * * * * /usr/local/bin/news-crawler feeds /app/data/feeds_de.txt --timezone $CRAWLER_TIMEZONE --lang $CRAWLER_LANGUAGE --dir /app/out/feeds' > crontab.tmp \
+    && echo '0 2 * * * /usr/local/bin/news-crawler scrape /app/out/feeds --timezone $CRAWLER_TIMEZONE --lang $CRAWLER_LANGUAGE --dir /app/out/articles' >> crontab.tmp \
     && crontab crontab.tmp \
     && rm -rf crontab.tmp
 
--- scraper/extract.go
+++ scraper/extract.go
@@ -2,9 +2,7 @@ package scraper
 
 import (
 	"bytes"
-	"fmt"
 	"net/url"
-	"unicode/utf8"
 
 	"github.com/advancedlogic/GoOse"
 	"github.com/jlubawy/go-boilerpipe"
@@ -13,7 +11,7 @@ import (
 
 // Extract the content of an article
 func (article *Article) Extract() error {
-	content, err := ExtractContentGoOseUtf8(article.FeedItem.URL, article.HTML)
+	content, err := ExtractContentGoOse(article.FeedItem.URL, article.HTML)
 	// content, err := ExtractContentBoilerpipe(article.FeedItem.URL, article.HTML)
 	if err != nil {
 		return err
@@ -23,47 +21,17 @@ func (article *Article) Extract() error {
 	return nil
 }
 
-var issues int
-
 func ExtractContentGoOse(url string, html string) (string, error) {
-	validUTF8 := utf8.Valid([]byte(html))
-	if !validUTF8 {
-		issues++
-		fmt.Println(issues)
-		// fmt.Println("No UTF8")
-	}
 	g := goose.New()
 	article, err := g.ExtractFromRawHTML(url, html)
 	if err != nil {
 		return "", err
-	}
-
-	return article.CleanedText, nil
-}
-
-func ExtractContentGoOseUtf8(url string, html string) (string, error) {
-	g := goose.New()
-	article, err := g.ExtractFromRawHTML(url, html)
-	if err != nil {
-		return "", err
-	}
-
-	// goose.UTF8encode(article.CleanedText, sourceCharset string)
-
-	validUTF8 := utf8.Valid([]byte(article.CleanedText))
-	if !validUTF8 {
-		fmt.Println("No UTF8")
 	}
 
 	return article.CleanedText, nil
 }
 
 func ExtractContentBoilerpipe(urlStr string, html string) (string, error) {
-	validUTF8 := utf8.Valid([]byte(html))
-	if !validUTF8 {
-		fmt.Println("No UTF8")
-	}
-
 	reader := bytes.NewReader([]byte(html))
 	url, err := url.Parse(urlStr)
 	if err != nil {
